589ddf46e07080388b7006a721c2c0a840d9c070
jlong0517 <jlong0517@hotmail.com>
2020-12-06 20:46:48 -0500

    Login
    Login for users (wireframe)

---
+++ Login/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.txtupass = new System.Windows.Forms.TextBox();
+            this.txtuname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(286, 293);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 17);
+            this.lblMessage.TabIndex = 20;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(354, 209);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(93, 41);
+            this.btnlogin.TabIndex = 19;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            // 
+            // txtupass
+            // 
+            this.txtupass.Location = new System.Drawing.Point(366, 157);
+            this.txtupass.Name = "txtupass";
+            this.txtupass.PasswordChar = '*';
+            this.txtupass.Size = new System.Drawing.Size(153, 22);
+            this.txtupass.TabIndex = 18;
+            // 
+            // txtuname
+            // 
+            this.txtuname.Location = new System.Drawing.Point(366, 121);
+            this.txtuname.Name = "txtuname";
+            this.txtuname.Size = new System.Drawing.Size(153, 22);
+            this.txtuname.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(286, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(283, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Username:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.txtupass);
+            this.Controls.Add(this.txtuname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Josh\'s Marketplace";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblMessage;
+        protected System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.TextBox txtupass;
+        private System.Windows.Forms.TextBox txtuname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Login/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        protected void btnlogin_Click(object sender, EventArgs e)
+        {
+            if (txtuname.Text == "")
+            {
+                lblMessage.Text = "Enter UserName";
+            }
+            else if (txtupass.Text == "")
+            {
+                lblMessage.Text = "Enter Password";
+            }
+            else
+            {
+                if (txtuname.Text == "jlong" && txtupass.Text == "winter")
+                {
+                    lblMessage.Text = "Login Success";
+                }
+                else
+                {
+                    lblMessage.Text = "Invalid Name or Password";
+                }
+            }
+        }
+    }
+}
+
